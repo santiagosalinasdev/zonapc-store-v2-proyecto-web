13125e82ddbabebe2ca457085d7ace4b62ba2440
Santiago Salinas Garzon <ssalinas@uc.edu.co>
2026-06-01 20:25:36 -0500

    Proyecto final Programación Web - ZonaPC Store V2
    Se agrega el proyecto final del módulo de Programación Web denominado ZonaPC Store V2, desarrollado con HTML, CSS y JavaScript. El proyecto incluye visualización de productos tecnológicos, simulación de compra, métodos de pago y generación de factura electrónica básica.

---
+++ logica.js
@@ -0,0 +1,79 @@
+let producto = '';
+let precio = 0;
+
+function mensaje() {
+    document.getElementById('productos')
+        .scrollIntoView({
+            behavior: 'smooth'
+        });
+}
+
+function comprar(nombre, valor) {
+    producto = nombre;
+    precio = valor;
+
+    alert(`Producto seleccionado:
+${producto}
+Precio: $${precio.toLocaleString('es-CO')}`);
+}
+
+function facturar() {
+
+    const nombre = document
+        .getElementById('nombre')
+        .value.trim();
+
+    const correo = document
+        .getElementById('correo')
+        .value.trim();
+
+    const envio = document
+        .getElementById('envio')
+        .value;
+
+    const metodo = document
+        .getElementById('metodo')
+        .value;
+
+    const validarCorreo =
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!nombre || !correo || !envio || !metodo) {
+        alert('Completa todos los campos');
+        return;
+    }
+
+    if (!validarCorreo.test(correo)) {
+        alert('Ingrese un correo válido');
+        return;
+    }
+
+    if (!producto) {
+        alert('Selecciona un producto antes de comprar');
+        return;
+    }
+
+    const iva = precio * 0.19;
+    const total = precio + iva;
+
+    document.getElementById('factura').innerHTML = `
+        <h2>🧾 FACTURA FINAL</h2>
+        <hr><br>
+
+        <p><strong>Cliente:</strong> ${nombre}</p>
+        <p><strong>Correo:</strong> ${correo}</p>
+        <p><strong>Producto:</strong> ${producto}</p>
+        <p><strong>Subtotal:</strong> $${precio.toLocaleString('es-CO')}</p>
+        <p><strong>IVA (19%):</strong> $${iva.toLocaleString('es-CO')}</p>
+        <p><strong>Total:</strong> $${total.toLocaleString('es-CO')}</p>
+        <p><strong>Envío:</strong> ${envio}</p>
+        <p><strong>Método de pago:</strong> ${metodo}</p>
+
+        <h3>✅ Compra Exitosa</h3>
+    `;
+
+    document.getElementById('nombre').value = '';
+    document.getElementById('correo').value = '';
+    document.getElementById('envio').value = '';
+    document.getElementById('metodo').value = '';
+}
